84763e7d77dca4143e441a2b3a52f9ef58f4b3ed
ShivangiRai1310 <shivangirai1310@gmail.com>
2020-07-14 02:54:23 +0530

    files removed

--- index.js
+++ index.js
@@ -41,20 +41,6 @@ con.connect(function (err) {
         console.log("Connected to dbms");
 });
 
-// function DMY() {
-//         var year, month, day;
-//         var monthShortNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-//         year = String(this.getFullYear());
-//         month = String(this.getMonth());
-//         // if (month.length == 1) {
-//         //     month = "0" + month;
-//         // }
-//         day = String(this.getDate());
-//         if (day.length == 1) {
-//             day = "0" + day;
-//         }
-//         return day + " " + monthShortNames[month] + " " + year;
-// };
 
 // INDEX PAGE
 app.get("/", authController.isLoggedIn, function (req, res) {
--- login signup/register.js
+++
@@ -1,7 +0,0 @@
-function matchPassword()
-{
-	if(document.getElementById('r7').value!=document.getElementById('r4').value)
-	{
-		alert("Passwords don't match, Enter again" );
-	}
-}
